9b455fa7af2711202fc219f4542200468acd37cd
rachchanon-la <rachchanon_la@cmu.ac.th>
2026-02-06 23:35:39 +0700

    Update lab16_1.cpp

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,34 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void randData(double *p, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        p[i] = (double)rand() / RAND_MAX;
+    }
+}
+void showData(double *p, int row, int col){
+    cout << fixed << setprecision(2);
+
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            cout << *(p + i*col + j) << " ";
+        }
+        cout << endl;
+    }
+}
+void findRowSum(const double *p, double *sum, int N, int M){
+    for(int i = 0; i < N; i++){
+        sum[i] = 0;
+        for(int j = 0; j < M; j++){
+            sum[i] += *(p + i*M + j);
+        }
+    }
+}
+void findColSum(const double *p, double *sum, int N, int M){
+    for(int j = 0; j < M; j++){
+        sum[j] = 0;
+        for(int i = 0; i < N; i++){
+            sum[j] += *(p + i*M + j);
+        }
+    }
+}
